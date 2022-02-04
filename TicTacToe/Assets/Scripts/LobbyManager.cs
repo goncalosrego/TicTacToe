1bcf7f5818bf771aafe576832d389ba1d0785e88
Gonçalo Rego <goncalo.rego@exaud.com>
2022-02-04 17:03:52 +0000

    Lobby Scene: Player movement and collision detection to enable the start game UI;

---
+++ TicTacToe/Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyManager : MonoBehaviour
+{
+    [SerializeField] PlayerMovement playerMovement;
+    [SerializeField] GameObject playGameUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playGameUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerMovement.PlayerCollided())
+        {
+            EnablePlayGameUI();
+        }
+    }
+
+    void EnablePlayGameUI()
+    {
+        playGameUI.SetActive(true);
+    }
+
+    public void StartGame()
+    {
+
+    }
+}
---
+++ TicTacToe/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Transform cachedTransform;
+    string horizontalName = "Horizontal";
+    string verticalName = "Vertical";
+    string detectionZoneTag = "DetectionZone";
+    float moveSpeed = 5.0f;
+    bool playerCollided = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cachedTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(PlayerCollided() == false)
+        {
+            MovePlayer();
+        }
+    }
+
+    void MovePlayer()
+    {
+        float horizontal = Input.GetAxis(horizontalName);
+        float vertical = Input.GetAxis(verticalName);
+
+        Vector3 movement = new Vector3(horizontal, 0f, vertical);
+        cachedTransform.Translate(movement * moveSpeed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == detectionZoneTag)
+        {
+            playerCollided = true;
+        }
+        else
+        {
+            playerCollided = false;
+        }
+    }
+
+    public bool PlayerCollided()
+    {
+        return playerCollided;
+    }
+}
