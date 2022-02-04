fc2e2d6b096a6c0e44cc3483490029fdd1ba08b3
Gonçalo Rego <goncalo.rego@exaud.com>
2022-02-04 15:57:36 +0000

    Login Scene UI;

--- TicTacToe/Assets/Scripts/LoginManager.cs
+++ TicTacToe/Assets/Scripts/LoginManager.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class LoginManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] TMP_InputField usernameInputField;
+    [SerializeField] TMP_InputField passwordInputField;
 
-    // Update is called once per frame
-    void Update()
+    public void LoginUser()
     {
-        
+        if(usernameInputField.text != "" && passwordInputField.text != "")
+        {
+        }
+        else
+        {
+            Debug.Log("Username or Password empty!");
+        }
     }
 }
