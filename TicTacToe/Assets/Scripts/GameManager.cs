c69d0a2ed8a22e80b1ae0890ad10fa20c383268a
Gonçalo Rego <goncalo.rego@exaud.com>
2022-02-07 11:32:50 +0000

    Scene Layout and pieces instantiation;

---
+++ TicTacToe/Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] GameObject xPiece;
+    Vector2 pieceStartingPosition = new Vector2(-7f, -6f);
+    Vector2 pieceOffset = new Vector2(3f, 0f);
+    int numberOfGamePieces = 6;
+
+    void Start()
+    {
+        InstantiateGamePieces();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void InstantiateGamePieces()
+    {
+        for(int i = 0; i < numberOfGamePieces; i++)
+        {
+            if(i == 0)
+            {
+                Instantiate(xPiece, pieceStartingPosition, Quaternion.identity);
+            }
+            else
+            {
+                Instantiate(xPiece, pieceStartingPosition + pieceOffset, Quaternion.identity);
+                pieceStartingPosition += pieceOffset;
+            }
+        }
+    }
+}
